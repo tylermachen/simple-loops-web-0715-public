a21fef2535bca2d6ae78364b9f1cbdd61caa1a02
Tyler Machen <tylermachen@gmail.com>
2015-07-30 09:28:35 -0400

    loop test complete and passing local build

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,19 +10,34 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  i = 0
+  loop do
+    puts phrase
+    i += 1
+    break if i == number_of_times
+  end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+
+  number_of_times.times do 
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+
+  i = 1
+  while i <= number_of_times
+    puts phrase
+
+    i +=1 
+  end
 end
 
 def until_iterator(number_of_times)
@@ -30,6 +45,12 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
+  i = 0
+  until i == number_of_times
+    puts phrase
+
+    i += 1
+  end
 end
 
 def for_iterator(number_of_times)
@@ -38,5 +59,8 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  for i in 1..number_of_times
+    puts phrase
+  end
 end
 
